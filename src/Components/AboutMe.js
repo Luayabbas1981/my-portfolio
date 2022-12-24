efdd7a8506639f1865802ff29d61fd4784ace9f4
Luay Abbas <abbasluay1981@gmail.com>
2022-12-24 23:10:19 +0100

    about me

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -1,12 +1,10 @@
-import React,{useState,useEffect} from 'react'
+import React,{useState} from 'react'
 import "./AboutMe.css"
 
 function AboutMe() {
   const [pageNum,setPageNum]= useState(1)
 
-  useEffect(()=>{
-    
-  },[pageNum])
+ 
   
   
   function firstLayer(){
