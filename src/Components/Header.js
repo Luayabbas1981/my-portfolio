ccde9895796dbf0b5ae57cd044ad1d2166ab9985
Luay Abbas <abbasluay1981@gmail.com>
2023-01-27 08:24:13 +0100

    fix pixel

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -33,9 +33,9 @@ function Header() {
   return (
    <>
    <header>
-    <div onClick={homeHandler} style={{color:`${home? " #47afff":"#c0bfbf"}`}}>Projects</div>
-    <div onClick={aboutMeHandler} style={{color:`${aboutMe? " #47afff":"#c0bfbf"}`}}>About me</div>
-    <div onClick={contactHandler  } style={{color:`${contact? " #47afff":"#c0bfbf"}`}}>Contact</div>
+    <div onClick={homeHandler} style={{color:`${home? " #36a5fb":"#c0bfbf"}`}}>Projects</div>
+    <div onClick={aboutMeHandler} style={{color:`${aboutMe? " #36a5fb":"#c0bfbf"}`}}>About me</div>
+    <div onClick={contactHandler  } style={{color:`${contact? " #36a5fb":"#c0bfbf"}`}}>Contact</div>
    </header>
    </>
   )
